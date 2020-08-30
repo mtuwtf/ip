ac89096c022e0681090b434fb71691842e03aea6
mtu <mtu@mtu.wtf>
2020-08-30 17:53:27 -0400

    add image directory and copy background image to it with the proper permissions

--- ip/src/www.sh
+++ ip/src/www.sh
@@ -8,6 +8,8 @@ fi
 
 FILE=$1
 
+doas mkdir -p /var/www/htdocs/i
+doas cp bg.jpg /var/www/htdocs/i/ && doas chmod 444 /var/www/htdocs/i/bg.jpg && doas chown www:www /var/www/htdocs/i/bg.jpg
 doas install -o www -g www -m 0500 ${FILE} /var/www/cgi-bin
 doas install -o www -g www -m 0444 ${FILE}.html /var/www/htdocs
 doas rm -f ${FILE}.o
